f16cd814d24a5aa057220ba490a5108b80ba7c05
LuisGomes18 <luiscgomes0@gmail.com>
2023-11-30 00:35:58 +0000

    Update de formatação

--- login.py
+++ login.py
@@ -4,23 +4,24 @@ from extras import carregar_dados
 
 def fazer_login(numero_conta, password, contas):
     for conta in contas:
-        if conta['numero'] == numero_conta and conta['password']:
+        if conta["numero"] == numero_conta and conta["password"]:
             return True
     return False
+
 
 def login_conta():
     try:
         dados = carregar_dados()
-        contas = dados['contas']
+        contas = dados["contas"]
 
-        numero_conta = input('Numero de conta: ')
-        password = input('Password: ')
+        numero_conta = input("Numero de conta: ")
+        password = input("Password: ")
 
         if fazer_login(numero_conta, password, contas):
-            print('\nAcess Granted\n')
+            print("\nAcess Granted\n")
         else:
-            print('\nAcess Denied\n')
+            print("\nAcess Denied\n")
     except FileNotFoundError:
-        print('Ficheiro com dados das contas nao encontrado')
+        print("Ficheiro com dados das contas nao encontrado")
     except JSONDecodeError:
-        print('Erro na formatação do contas.json')
+        print("Erro na formatação do contas.json")
