e4f2568cbab6d861642728a363ee0f03b4ffdce3
LuisGomes18 <luiscgomes0@gmail.com>
2023-11-30 00:54:26 +0000

    Remoção dos docstring

--- extras.py
+++ extras.py
@@ -1,41 +1,11 @@
-"""
-Módulo para carregar dados de um arquivo JSON e manipular o sistema operacional.
-
-Este módulo contém duas funções principais:
-
-1. `carregar_dados`: Carrega os dados de um arquivo JSON.
-    - Tenta abrir o arquivo "data/data.json" e carregar os dados.
-    - Retorna um dicionário contendo os dados carregados do arquivo JSON.
-    - Exceções:
-        - JSONDecodeError: Se houver um erro na formatação do arquivo JSON.
-        - FileNotFoundError: Se o arquivo não for encontrado.
-        - Exception: Para outros erros inesperados durante o carregamento dos dados.
-
-2. `sistema_operativo`: Limpa a tela do console com base no sistema operacional.
-    - Detecta o sistema operacional e utiliza o comando apropriado para limpar a tela do console.
-    - Exceção:
-        - OSError: Se ocorrer um erro ao tentar limpar a tela do console.
-"""
 from json import load
+from json import dump
 from json import JSONDecodeError
 import platform
 import os
 
 
 def carregar_dados():
-    """
-    Carrega os dados de um arquivo JSON.
-
-    Tenta abrir o arquivo "data/data.json" e carregar os dados.
-
-    Returns:
-        dict: Um dicionário contendo os dados carregados do arquivo JSON.
-
-    Raises:
-        JSONDecodeError: Se houver um erro na formatação do arquivo JSON.
-        FileNotFoundError: Se o arquivo não for encontrado.
-        Exception: Para outros erros inesperados durante o carregamento dos dados.
-    """
     try:
         with open("data/login.json", "r", encoding="utf-8") as dd:
             dados = load(dd)  # type: ignore
@@ -49,15 +19,15 @@ def carregar_dados():
         return dados
 
 
+def guardar_dados(dados):
+    try:
+        with open("data/login.json", "w", encoding="utf-8") as dd:
+            dump(dados, dd)
+    except Exception as erro:  # pylint: disable=broad-except
+        print(f"ERRO ao guardar dados: {erro}")
+
+
 def apagar_terminal():
-    """
-    Limpa a tela do console com base no sistema operacional.
-
-    Detecta o sistema operacional e utiliza o comando apropriado para limpar a tela do console.
-
-    Raises:
-        OSError: Se ocorrer um erro ao tentar limpar a tela do console.
-    """
     try:
         sistema_operacional = platform.system()
 
--- login.py
+++ login.py
@@ -1,8 +1,28 @@
+"""login module
+
+This module implements a basic login system.
+
+Functions:
+- fazer_login(numero_conta, password, contas): Checks if login credentials are valid.
+- login_conta(): Facilitates the user login process.
+
+"""
 from json import JSONDecodeError
 from extras import carregar_dados
 
 
 def fazer_login(numero_conta, password, contas):
+    """Check if login credentials are valid.
+
+    Args:
+    numero_conta (str): The account number to be checked.
+    password (str): The password to be checked.
+    contas (list): A list of dictionaries containing account information.
+
+    Returns:
+    bool: True if login is successful, False otherwise.
+
+    """
     for conta in contas:
         if conta["numero"] == numero_conta and conta["password"]:
             return True
@@ -10,6 +30,19 @@ def fazer_login(numero_conta, password, contas):
 
 
 def login_conta():
+    """Facilitate the user login process.
+
+    This function prompts the user for account number and password,
+    then checks if the login credentials are valid using the fazer_login function.
+
+    Prints:
+    str: A message indicating whether access is granted or denied.
+
+    Raises:
+    FileNotFoundError: If the file with account data is not found.
+    JSONDecodeError: If there is an error in the formatting of the contas.json file.
+
+    """
     try:
         dados = carregar_dados()
         contas = dados["contas"]
